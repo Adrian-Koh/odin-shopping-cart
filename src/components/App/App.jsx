df13e86ece0df0bbbf7b558ce7fc1b14f785b3e1
Adrian-Koh <adriankoh216@yahoo.com>
2025-07-19 16:36:09 +0800

    Add background image

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -6,10 +6,24 @@ function App() {
   return (
     <>
       <nav className={styles.navBar}>
-        <Link to="home">Home</Link>
-        <Link to="shop">Shop</Link>
+        <Link to="home" className={styles.navBarLink}>
+          Home
+        </Link>
+        <Link to="shop" className={styles.navBarLink}>
+          Shop
+        </Link>
       </nav>
       <Outlet></Outlet>
+      <footer>
+        Photo by{" "}
+        <a href="https://unsplash.com/@sphotos_00?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">
+          Sven
+        </a>{" "}
+        on{" "}
+        <a href="https://unsplash.com/photos/photo-of-house-at-meadow-V7WkmXntA8M?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">
+          Unsplash
+        </a>
+      </footer>
     </>
   );
 }
