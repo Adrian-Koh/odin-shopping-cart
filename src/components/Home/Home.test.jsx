c88463d3dbb1fd6fef033a4078e2f6435802768d
Adrian-Koh <adriankoh216@yahoo.com>
2025-07-21 22:53:04 +0800

    Add test for image rendered

--- src/components/Home/Home.test.jsx
+++ src/components/Home/Home.test.jsx
@@ -1,11 +1,26 @@
 import { describe, it, expect } from "vitest";
 import { render, screen } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
 import { Home } from "./Home";
 
 describe("Home component", () => {
   it("renders home", () => {
     const { container } = render(<Home />);
     expect(container).toMatchSnapshot();
+  });
+
+  it("renders heading", () => {
+    render(<Home />);
+    expect(screen.getByRole("heading").textContent).toMatch(
+      /The Everything Shop/i,
+    );
+  });
+
+  it("renders image with correct source path", () => {
+    render(<Home />);
+    const image = screen.getByRole("img");
+    expect(image).toHaveAttribute(
+      "src",
+      "../../../public/old-man-portrait.jpg",
+    );
   });
 });
